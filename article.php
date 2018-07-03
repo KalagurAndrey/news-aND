4badedd36f20bf2614df204b73655c6e78f6c6d3
KalagurAndrey <a.kalagur@robo.finance>
2018-07-03 15:21:45 +0700

    version 4

---
+++ article.php
@@ -0,0 +1,22 @@
+<?
+include_once 'includes/header.php';
+include_once 'settings/db.php';
+
+?>
+
+<div class="middle">
+    <div class="container">
+        <div class="content">
+
+            <h3>Статьи</h3>
+
+
+
+
+        </div>
+    </div>
+</div>
+    <?php
+    include_once 'includes/sidebar.php';
+    include_once 'includes/footer.php';
+    ?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,16 @@
 <div class="right-sidebar">
-    Посты недели
+    <br><br><br><br>  <h3> Посты недели:</h3><br>
+    <?php
+    $select = mysqli_query($CONNECT, "SELECT * FROM news WHERE date > NOW() - INTERVAL 7 DAY ORDER BY views DESC LIMIT 10 ");
+
+    while($result = mysqli_fetch_array($select)) {
+        echo '<div class="top-ten">';
+        echo $result['caption'];
+        echo '</div><br>';
+   ;
+    }
+
+    ?>
 </div>
 
 </div>
